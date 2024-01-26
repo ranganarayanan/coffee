08bed41596995434511c1a647ddc32dcae923825
ranganarayanan <ranganarayanana2002@gmail.com>
2024-01-26 14:46:29 +0530

    item page

--- app/src/main/java/com/example/coffee/Page2.java
+++ app/src/main/java/com/example/coffee/Page2.java
@@ -2,13 +2,25 @@ package com.example.coffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Page2 extends AppCompatActivity {
-
+ImageView esarrrow;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+        esarrrow=(ImageView) findViewById(R.id.esparrow);
+        esarrrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Page2.this,Espresso.class));
+            }
+        });
     }
 }
