a1e0a75ae302640b11dbc6b216602c422787527a
ranganarayanan <ranganarayanana2002@gmail.com>
2024-01-27 15:51:25 +0530

    adding elements

--- app/src/main/java/com/example/coffee/Cappuccino.java
+++ app/src/main/java/com/example/coffee/Cappuccino.java
@@ -2,13 +2,25 @@ package com.example.coffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Cappuccino extends AppCompatActivity {
+    ImageView bckarrow;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cappuccino);
+        bckarrow=(ImageView) findViewById(R.id.imbackarrow);
+        bckarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
