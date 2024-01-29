bedaedddcea978cc85627f6b9af1c4aeefb1cfeb
ranganarayanan <ranganarayanana2002@gmail.com>
2024-01-29 10:43:23 +0530

    adding elements

--- app/src/main/java/com/example/coffee/Macchiato.java
+++ app/src/main/java/com/example/coffee/Macchiato.java
@@ -2,13 +2,55 @@ package com.example.coffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Macchiato extends AppCompatActivity {
+    ImageView bckarrow;
+    ImageView min;
+    ImageView max;
+    TextView con;
+    TextView rsp;
+    int count=1;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_macchiato);
+        bckarrow=(ImageView) findViewById(R.id.imbackarrow);
+        bckarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        min=(ImageView) findViewById(R.id.minus);
+        max=(ImageView) findViewById(R.id.add);
+        con=(TextView) findViewById(R.id.count);
+        rsp=(TextView) findViewById(R.id.rs);
+        max.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count=count+1;
+                con.setText(""+count);
+                rsp.setText(""+(count*295));
+            }
+        });
+        min.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count=count-1;
+                if(count<=1)
+                    count=1;
+                con.setText(""+count);
+                rsp.setText(""+(count*295));
+
+            }
+        });
+
     }
 }
